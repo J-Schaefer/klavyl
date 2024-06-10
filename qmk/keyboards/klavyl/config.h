4c7d690cc813372ceaed7cb8c4eef9fc3a6f538e
Jeroen Schäfer <jeroen.schaefer@outlook.de>
2024-06-10 22:39:32 +0200

    Change keymap and change TAPPING_TERM
    Added another layer with a numpad. Also set
    TAPPING_TERM to 220 ms.

--- qmk/keyboards/klavyl/config.h
+++ qmk/keyboards/klavyl/config.h
@@ -6,3 +6,6 @@
 #define SERIAL_USART_TX_PIN GP0  // USART TX pin
 #define SERIAL_USART_RX_PIN GP1  // USART RX pin
 #define SERIAL_USART_PIN_SWAP      // Swap TX and RX pins if keyboard is master halve. (Only available on some MCUs)
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 220
